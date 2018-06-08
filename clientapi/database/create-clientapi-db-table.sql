3e421427785035b6e32f5c25425fca8383c63758
Bingham <019631@aero.local>
2018-06-08 10:33:19 -0400

    adding the database and initial table setup for MySQL

---
+++ clientapi/database/create-clientapi-db-table.sql
@@ -0,0 +1,12 @@
+CREATE TABLE clients (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    address VARCHAR(300) NOT NULL,
+    city VARCHAR(100) NOT NULL,
+    state VARCHAR(50) NOT NULL,
+    zip VARCHAR(10) NOT NULL,
+    web VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    phone VARCHAR(12) NOT NULL,
+    created VARCHAR(100) NOT NULL
+);
